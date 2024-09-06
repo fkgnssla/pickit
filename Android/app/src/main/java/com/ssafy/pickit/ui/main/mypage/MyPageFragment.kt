b6db320af6e52bedc403c1bb6f20b29d2f166cd8
김민철 <mckim0223@naver.com>
2024-09-06 14:54:00 +0900

    Fix: 지갑 생성동의 페이지 수정

--- Android/app/src/main/java/com/ssafy/pickit/ui/main/mypage/MyPageFragment.kt
+++ Android/app/src/main/java/com/ssafy/pickit/ui/main/mypage/MyPageFragment.kt
@@ -3,7 +3,9 @@ package com.ssafy.pickit.ui.main.mypage
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.CheckBox
 import android.widget.EditText
+import android.widget.TextView
 import androidx.fragment.app.viewModels
 import androidx.fragment.app.Fragment
 import androidx.appcompat.app.AlertDialog
@@ -33,7 +35,6 @@ class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_
             showMemberModifyDialog()
         }
 
-        // member_modify 버튼 클릭 시 수정 화면을 다이얼로그로 띄우는 리스너 설정
         binding.tmpWallet.setOnClickListener {
             showTmpWalletDialog()
         }
@@ -92,36 +93,53 @@ class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_
         println("비밀번호 확인: $newPasswordConfirm")
     }
 
-    private fun showTmpWalletDialog(){
+    private fun showTmpWalletDialog() {
         val inflater = LayoutInflater.from(requireContext())
         val dialogView = inflater.inflate(R.layout.wallet_agreement, null)
+        val nowId = "ssafy123"
 
         // AlertDialog 빌더 사용
         val dialogBuilder = AlertDialog.Builder(requireContext())
-            .setTitle("지갑 생성 동의 내역서")
+//            .setTitle("지갑 생성 동의 내역서")
             .setView(dialogView)
-            .setPositiveButton("저장") { dialog, _ ->
-                // 수정 내용 처리
-                val walletAgreeId = dialogView.findViewById<EditText>(R.id.walletAgreeId).text.toString()
-                val walletIdx = dialogView.findViewById<EditText>(R.id.walletIdx).text.toString()
-
-                // 여기서 입력한 내용을 처리하거나 viewModel을 통해 데이터 업데이트
-                handleWalletGenerate(walletAgreeId, walletIdx)
-                dialog.dismiss()
-            }
+            .setPositiveButton("저장", null) // null로 설정하고 나중에 버튼을 활성화/비활성화
             .setNegativeButton("취소") { dialog, _ ->
                 dialog.cancel()
             }
             .setCancelable(false)
-        // 다이얼로그를 생성하고 표시
+
         val dialog = dialogBuilder.create()
         dialog.show()
 
+        // PositiveButton 찾기
+        val positiveButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE)
+        positiveButton.isEnabled = false // 초기에는 비활성화
+
+        val checkBox = dialogView.findViewById<CheckBox>(R.id.checkBox)
+
+        // CheckBox가 체크될 때 PositiveButton 활성화/비활성화
+        checkBox.setOnCheckedChangeListener { _, isChecked ->
+            positiveButton.isEnabled = isChecked
+            positiveButton.setTextColor(
+                if (isChecked) resources.getColor(android.R.color.holo_purple)
+                else resources.getColor(android.R.color.darker_gray)
+            )
+        }
+
+        // 저장 버튼 클릭 리스너 설정
+        positiveButton.setOnClickListener {
+            val walletAgreeId = dialogView.findViewById<EditText>(R.id.walletAgreeId).text.toString()
+            val walletNum = dialogView.findViewById<TextView>(R.id.walletNum).text.toString()
+
+            handleWalletGenerate(walletAgreeId, walletNum)
+            dialog.dismiss()
+        }
     }
 
-    private fun handleWalletGenerate(walletAgreeId: String, walletIdx:String){
+
+    private fun handleWalletGenerate(walletAgreeId: String, walletNum:String){
         println("동의 id: $walletAgreeId")
-        println("지갑번호: $walletIdx")
+        println("지갑번호: $walletNum")
     }
 }
 
