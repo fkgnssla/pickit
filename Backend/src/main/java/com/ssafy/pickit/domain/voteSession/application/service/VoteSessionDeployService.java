2c0549cd0b8ebcddd9b85bdc6fec9ac3820de589
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2024-09-16 13:00:08 +0900

    Feat : RestTemplate 빈 등록

--- Backend/src/main/java/com/ssafy/pickit/domain/voteSession/application/service/VoteSessionDeployService.java
+++ Backend/src/main/java/com/ssafy/pickit/domain/voteSession/application/service/VoteSessionDeployService.java
@@ -19,20 +19,22 @@ import org.springframework.web.client.RestTemplate;
 import com.ssafy.pickit.domain.tempVoteSession.domain.TempCandidate;
 import com.ssafy.pickit.domain.voteSession.dto.VoteSessionDeployResponse;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
 @EnableAsync
+@RequiredArgsConstructor
 public class VoteSessionDeployService {
+
+	private final RestTemplate restTemplate;
 
 	@Value("${vote-session.deploy.url}")
 	private String url;
 
 	@Async
 	public CompletableFuture<String> deploy(List<TempCandidate> candidates) {
-		RestTemplate restTemplate = new RestTemplate();
-
 		// 요청 헤더 설정
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
