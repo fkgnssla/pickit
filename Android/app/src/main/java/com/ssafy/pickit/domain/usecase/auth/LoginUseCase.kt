79f3cdfb4bc382097881c835caee7c34413cbbb2
rkdmf1026 <50603273+rkdmf1026@users.noreply.github.com>
2024-09-23 17:58:43 +0900

    Rename : LoginUseCase 이름변경

--- Android/app/src/main/java/com/ssafy/pickit/domain/usecase/auth/LoginUseCase.kt
+++ Android/app/src/main/java/com/ssafy/pickit/domain/usecase/auth/LoginUseCase.kt
@@ -1,11 +1,11 @@
-package com.ssafy.pickit.domain.usecase
+package com.ssafy.pickit.domain.usecase.auth
 
 import com.ssafy.pickit.domain.entity.LogInItem
 import com.ssafy.pickit.domain.entity.LoginData
 import com.ssafy.pickit.domain.repository.AuthRepository
 import javax.inject.Inject
 
-class AuthUseCase @Inject constructor(
+class LoginUseCase @Inject constructor(
     private val authRepository: AuthRepository
 ) {
     suspend operator fun invoke(logInItem: LogInItem): LoginData =
