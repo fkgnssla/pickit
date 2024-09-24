049d363b190524d9cb71694eb357e587447d3ce8
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2024-09-24 10:25:11 +0900

    Refactor : DTO 타입 record로 변경, 응답 형식 변경

--- Backend/src/main/java/com/ssafy/pickit/domain/vote/api/VoteController.java
+++ Backend/src/main/java/com/ssafy/pickit/domain/vote/api/VoteController.java
@@ -1,6 +1,5 @@
 package com.ssafy.pickit.domain.vote.api;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ssafy.pickit.domain.member.domain.PrincipalDetail;
 import com.ssafy.pickit.domain.vote.application.service.VoteProducer;
 import com.ssafy.pickit.domain.vote.dto.VoteRequest;
+import com.ssafy.pickit.global.response.ApiResponse;
+import com.ssafy.pickit.global.response.ResponseUtils;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,9 +22,9 @@ public class VoteController {
 	private final VoteProducer voteProducer;
 
 	@PostMapping
-	public ResponseEntity<?> createVote(@RequestBody VoteRequest voteRequest,
+	public ApiResponse<?> createVote(@RequestBody VoteRequest voteRequest,
 		@AuthenticationPrincipal PrincipalDetail principalDetail) {
 		voteProducer.submitVote(principalDetail.getId(), voteRequest);
-		return ResponseEntity.status(201).body("Vote Sended");
+		return ResponseUtils.success();
 	}
 }
--- Backend/src/main/java/com/ssafy/pickit/domain/vote/dto/VoteRequest.java
+++ Backend/src/main/java/com/ssafy/pickit/domain/vote/dto/VoteRequest.java
@@ -1,19 +1,13 @@
 package com.ssafy.pickit.domain.vote.dto;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
-@Getter
-@ToString
-@NoArgsConstructor
-public class VoteRequest {
-	private Long memberId;
-	private String voteSessionId;
-	private String candidateId;
-	private String transactionHash;
-
-	public void setMemberId(Long memberId) {
-		this.memberId = memberId;
+public record VoteRequest(
+	Long memberId,
+	String voteSessionId,
+	String candidateId,
+	String transactionHash
+) {
+	public VoteRequest withMemberId(Long newMemberId) {
+		return new VoteRequest(newMemberId, this.voteSessionId, this.candidateId, this.transactionHash);
 	}
 }
+
