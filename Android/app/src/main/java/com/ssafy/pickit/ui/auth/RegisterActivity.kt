c78389481e35bb3d139522fa43c516cca8fabecc
rkdmf1026 <50603273+rkdmf1026@users.noreply.github.com>
2024-09-24 12:39:33 +0900

    Feat : RegisterActivity 추가

---
+++ Android/app/src/main/java/com/ssafy/pickit/ui/auth/RegisterActivity.kt
@@ -0,0 +1,31 @@
+package com.ssafy.pickit.ui.auth
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.ssafy.pickit.R
+import com.ssafy.pickit.ui.main.MainActivity
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_register)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    companion object {
+        fun start(context : Context) {
+            val intent = Intent(context, RegisterActivity::class.java)
+            context.startActivity(intent)
+        }
+    }
+}
