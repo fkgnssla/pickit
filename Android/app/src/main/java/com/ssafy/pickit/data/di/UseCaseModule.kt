2d0f8c1f02b848baf63c0990f8b6bd342ea74c50
rkdmf1026 <50603273+rkdmf1026@users.noreply.github.com>
2024-09-26 17:44:53 +0900

    Feat : Hilt 모듈 추가

--- Android/app/src/main/java/com/ssafy/pickit/data/di/UseCaseModule.kt
+++ Android/app/src/main/java/com/ssafy/pickit/data/di/UseCaseModule.kt
@@ -1,9 +1,15 @@
 package com.ssafy.pickit.data.di
 
 import com.ssafy.pickit.domain.repository.AuthRepository
+import com.ssafy.pickit.domain.repository.VoteRepository
 import com.ssafy.pickit.domain.repository.WalletRepository
 import com.ssafy.pickit.domain.usecase.auth.LoginUseCase
 import com.ssafy.pickit.domain.usecase.auth.RegisterUseCase
+import com.ssafy.pickit.domain.usecase.vote.EndBroadcastVoteListUseCase
+import com.ssafy.pickit.domain.usecase.vote.EndVoteListUseCase
+import com.ssafy.pickit.domain.usecase.vote.OngoingBroadcastVoteListUseCase
+import com.ssafy.pickit.domain.usecase.vote.OngoingVoteListUseCase
+import com.ssafy.pickit.domain.usecase.vote.VoteUseCase
 import com.ssafy.pickit.domain.usecase.wallet.GenerateWalletUseCase
 import com.ssafy.pickit.domain.usecase.wallet.InsertWalletUseCase
 import dagger.Module
@@ -34,4 +40,35 @@ object UseCaseModule {
     @Provides
     fun provideInsertWalletUseCase(walletRepository: WalletRepository): InsertWalletUseCase =
         InsertWalletUseCase(walletRepository)
+
+    @Provides
+    @Singleton
+    fun provideEndBroadcastVoteListUseCase(voteRepository: VoteRepository): EndBroadcastVoteListUseCase {
+        return EndBroadcastVoteListUseCase(voteRepository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideEndVoteListUseCase(voteRepository: VoteRepository): EndVoteListUseCase {
+        return EndVoteListUseCase(voteRepository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideOngoingBroadcastVoteListUseCase(voteRepository: VoteRepository): OngoingBroadcastVoteListUseCase {
+        return OngoingBroadcastVoteListUseCase(voteRepository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideOngoingVoteListUseCase(voteRepository: VoteRepository): OngoingVoteListUseCase {
+        return OngoingVoteListUseCase(voteRepository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideVoteUseCase(voteRepository: VoteRepository): VoteUseCase {
+        return VoteUseCase(voteRepository)
+    }
+
 }
