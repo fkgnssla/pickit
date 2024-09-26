f1dc7db70cb3106fa706efbc16e0bf85d3884397
rkdmf1026 <50603273+rkdmf1026@users.noreply.github.com>
2024-09-26 17:34:45 +0900

    Fix : ResponseWrapper 수정

--- Android/app/src/main/java/com/ssafy/pickit/ui/auth/AuthViewModel.kt
+++ Android/app/src/main/java/com/ssafy/pickit/ui/auth/AuthViewModel.kt
@@ -9,6 +9,10 @@ import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.user.UserApiClient
 import com.ssafy.pickit.domain.entity.LogInItem
 import com.ssafy.pickit.domain.usecase.auth.LoginUseCase
+import com.ssafy.pickit.domain.usecase.vote.EndBroadcastVoteListUseCase
+import com.ssafy.pickit.domain.usecase.vote.EndVoteListUseCase
+import com.ssafy.pickit.domain.usecase.vote.OngoingBroadcastVoteListUseCase
+import com.ssafy.pickit.domain.usecase.vote.OngoingVoteListUseCase
 import com.ssafy.pickit.ui.main.MainActivity
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -16,7 +20,11 @@ import javax.inject.Inject
 
 @HiltViewModel
 class AuthViewModel @Inject constructor(
-    private val loginUseCase: LoginUseCase
+    private val loginUseCase: LoginUseCase,
+    private val ongoingBroadcastVoteListUseCase: OngoingBroadcastVoteListUseCase,
+    private val ongoingVoteListUseCase: OngoingVoteListUseCase,
+    private val endVoteListUseCase: EndVoteListUseCase,
+    private val endBroadcastVoteListUseCase: EndBroadcastVoteListUseCase
 ) : ViewModel() {
 
     private val _loginState = MutableLiveData<LoginState>(LoginState.LoadingState)
@@ -29,6 +37,15 @@ class AuthViewModel @Inject constructor(
             else _loginState.value = LoginState.NewUserState
         }
 
+    }
+
+    fun test() {
+        viewModelScope.launch {
+            val data1 = ongoingVoteListUseCase()
+            val data2 = ongoingBroadcastVoteListUseCase("66f132e160e1086af849100d")
+            val data3 = endVoteListUseCase()
+            val data4 = endBroadcastVoteListUseCase("66f132e160e1086af849100d")
+        }
     }
 
     val kakaoLoginCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
