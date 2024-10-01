fc56c3e5c743d80a23b3b1ac6704e5042313176b
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2024-10-01 16:41:46 +0900

    Feat : 회원 정보 조회

---
+++ Backend/Core/src/main/java/com/ssafy/pickit/domain/member/dto/MemberResponse.java
@@ -0,0 +1,17 @@
+package com.ssafy.pickit.domain.member.dto;
+
+import com.ssafy.pickit.domain.member.domain.Member;
+
+public record MemberResponse(
+	String name,
+	Integer age,
+	String gender
+) {
+	public static MemberResponse from(Member member) {
+		return new MemberResponse(
+			member.getName(),
+			member.getAge(),
+			member.getGender()
+		);
+	}
+}
