15eca197fe6af176e1dca6f91ed1df9d06553abe
김민철 <mckim0223@naver.com>
2024-10-04 17:42:55 +0900

    Fix: axios 수정

--- Frontend/Pickit/src/Done.tsx
+++ Frontend/Pickit/src/Done.tsx
@@ -1,15 +1,33 @@
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function Done() {
   const navigate = useNavigate();
   const home = async () => {
     navigate("/");
   };
+  const viewTempVotes = () =>{
+    axios({
+      method: 'get',
+      url: 'https://j11a309.p.ssafy.io/api/temp-vote-session',
+      withCredentials: true, 
+    })
+    .then((res) => {
+      console.log("결과");
+      console.log(res);
+    })
+    .catch((e) => {
+      console.log("결과보기실패");
+      console.log(e);
+    });
+  }
+
   return (
     <div>
       <h1>Done</h1>
       <h5>작성 내용을 심사 후 투표가 생성될 예정입니다.</h5>
       <h5 className="okButton" onClick={home}> 메인으로 </h5>
+      <button onClick={viewTempVotes}>등록 투표 보기</button>
     </div>
   );
 }
--- Frontend/Pickit/vite.config.ts
+++ Frontend/Pickit/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), mkcert()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://j11a309.p.ssafy.io',
+        changeOrigin: true,
+        secure: false, // HTTPS 요청이 아닌 경우 설정
+        rewrite: (path) => path.replace(/^\/api/, ''), // /api를 제거하고 실제 경로로 전달
+      },
+    },
+    https: true,  // HTTPS를 활성화
+    // port: 5173,   // 원하는 포트 설정
+  },
+});
