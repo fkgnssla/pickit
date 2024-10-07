307f06fc0805bc42abe52e21c7a026d40166371f
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2024-10-07 10:12:44 +0900

    Feat : birthday 속성 추가

--- Backend/Core/src/main/java/com/ssafy/pickit/domain/member/domain/Member.java
+++ Backend/Core/src/main/java/com/ssafy/pickit/domain/member/domain/Member.java
@@ -1,5 +1,7 @@
 package com.ssafy.pickit.domain.member.domain;
 
+import java.time.LocalDate;
+
 import com.ssafy.pickit.domain.auth.dto.SignUpRequest;
 import com.ssafy.pickit.domain.wallet.domain.Wallet;
 
@@ -32,7 +34,7 @@ public class Member {
 
 	private String name;
 
-	private int age;
+	private LocalDate birthday;
 
 	@Enumerated(EnumType.STRING)
 	private Gender gender;
@@ -42,10 +44,10 @@ public class Member {
 	@Enumerated(EnumType.STRING)
 	private Role role;
 
-	public static Member of(SignUpRequest signUpRequest, Wallet newWallet) {
+	public static Member of(SignUpRequest signUpRequest, Wallet newWallet, LocalDate birthday) {
 		return Member.builder()
 			.name(signUpRequest.name())
-			.age(signUpRequest.age())
+			.birthday(birthday)
 			.gender(Gender.valueOf(signUpRequest.gender().toUpperCase()))
 			.socialId(signUpRequest.socialId())
 			.wallet(newWallet)
